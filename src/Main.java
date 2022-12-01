6b2a70c363364d67d349d7915a531228ba9ee2cc
Anna Porvatova <anechka251520@mail.ru>
2022-12-01 21:42:27 +0300

    git commit -m "Initial commit"

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 200;
+        int c = 800;
+        int percent = (c / 100);
+        if (c >= 1000) {
+            percent = (c / 100);
+        } else {
+            percent = 0;
+        }
+        System.out.println(amount + c + percent);
+
+    }
+}
